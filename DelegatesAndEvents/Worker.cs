700a6c33b442675114f1129c5641a52226aa2512
Andrii.Ryzhyi <andrii.nrj@gmail.com>
2020-03-31 20:33:51 +0300

    Delegates and Events Examples

---
+++ DelegatesAndEvents/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
+    public class Worker
+    {
+        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler WorkCompleted;
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i + 1, workType);
+            }
+            OnWorkCompleted();
+            //Raise event
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            (WorkPerformed as WorkPerformedHandler)?.Invoke(hours, workType);
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            (WorkCompleted as EventHandler)?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}

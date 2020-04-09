cf95472b44f16d4acc243dc628ad771726757b8c
andrii-ryzhyi <32127294+andrii-ryzhyi@users.noreply.github.com>
2020-04-09 16:44:53 +0300

    Add LoginHistoryController

--- 6_LINQ_to_SQL/Controllers/LoginHistoryController.cs
+++ 6_LINQ_to_SQL/Controllers/LoginHistoryController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IteaLinqToSql.Models.Entities;
+using IteaLinqToSql.Models.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IteaLinqToSql.Controllers
 {
@@ -11,5 +14,47 @@ namespace IteaLinqToSql.Controllers
     [ApiController]
     public class LoginHistoryController : ControllerBase
     {
+        readonly IService<LoginHistory> service;
+
+        public LoginHistoryController(IService<LoginHistory> service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public List<LoginHistory> Get()
+        {
+            return service
+                .GetQuery()
+                .Include(x => x.User)
+                .ToList();
+        }
+
+        [HttpGet("{id}")]
+        public LoginHistory Get(int id)
+        {
+            return service.FindById(id);
+        }
+
+        [HttpPost("save")]
+        public List<LoginHistory> Post([FromBody] LoginHistory value)
+        {
+            return service
+                .GetAll()
+                .Where(x => x.UserId == value.UserId ||
+                            x.UserDevice.Contains(value.UserDevice) ||
+                            x.Id == value.Id)
+                .ToList();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
     }
 }

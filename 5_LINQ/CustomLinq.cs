7989b0f55aa004060bb1349581fbb52e82875300
andrii-ryzhyi <32127294+andrii-ryzhyi@users.noreply.github.com>
2020-04-13 16:58:51 +0300

    Updated

--- 5_LINQ/CustomLinq.cs
+++ 5_LINQ/CustomLinq.cs
@@ -8,17 +8,8 @@ namespace IteaLinq
         public static IEnumerable<T> CustomWhere<T>(this IEnumerable<T> collection, Predicate<T> predicate)
         {
             foreach (var item in collection)
-<<<<<<< HEAD
                 if (predicate(item))
                     yield return item;
-=======
-            {
-                if (predicate(item))
-                {
-                    yield return item;
-                }
-            }
->>>>>>> b98c0122454e40247b55ad6a0cd6f2bf3579d819
         }
     }
 }
